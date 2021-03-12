75bdc87352f605974002037fc328c8a006e67d7f
sttell <st.tell@mail.ru>
2021-03-12 23:06:04 +0300

    Update README.md | Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,12 @@
 import os
+import json as js
+
+
+def create_js(path):
+	file = open(path, 'w')
+	file.close()
+	return os.path.abspath(path)
+
 
 PATH = str(os.getcwd())			# full path from install directory
 
@@ -26,3 +34,37 @@ file.write(TEPMLATE_BASH)
 file.close()
 
 os.system(f'chmod 755 {BASH_FILE_PATH}')
+
+settings_path = create_js(PATH + './JS/settings.json')
+links_path = create_js(PATH + './JS/links.json')
+
+TEMPLATE_INCLUDE_PY = '''
+FULL_PATH_ASMR = '{}'
+
+SETTINGS_PATH = FULL_PATH_ASMR + '/JS/settings.json'
+DEFAULT_SETTINGS_PATH = FULL_PATH_ASMR + '/JS/dsettings.json'
+LINK_FILE_PATH = FULL_PATH_ASMR + '/JS/links.json'
+COMMANDS_MODULES_PATH = FULL_PATH_ASMR + '/Commands/'
+
+# DICT: {COMMAND_NAME: MODULE IN COMMANDS FOLDER}
+INCLUDES = {'sl': 'sl',
+		    'showlinks': 'sl',
+		    'nl':'nlink',
+		    'nlink': 'nlink',
+		    'newlink': 'nlink',
+		    'ed': 'edit',
+		    'edit': 'edit',
+		    'rm': 'rml',
+		    'rml': 'rml',
+		    'rmlink': 'rml',
+		    'sf': 'setflag',
+		    'setf': 'setflag',
+		    'setflag': 'setflag',
+		    'ss': 'showsettings',
+		    'showset': 'showsettings',
+		    'showsettings': 'showsettings',
+		    'settings': 'showsettings'}
+'''
+file = open('include.py', 'w')
+file.write(TEMPLATE_INCLUDE_PY.format(PATH))
+file.close()
